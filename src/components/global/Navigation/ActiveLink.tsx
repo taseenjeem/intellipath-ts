f7a6d52190f17aa605117df223e737d78da49421
taseenjeem <taseenjeem.dev@gmail.com>
2024-08-19 02:43:14 +0600

    Update ActiveLink.tsx

--- src/components/global/Navigation/ActiveLink.tsx
+++ src/components/global/Navigation/ActiveLink.tsx
@@ -1,11 +1,10 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
 
 interface ActiveLinkProps {
   href: string;
-  children: ReactNode;
+  children: React.ReactNode;
   isSmallDevice?: boolean;
 }
 
