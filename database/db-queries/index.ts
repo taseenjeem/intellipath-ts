583c07802a4d3fdd2187c7e603a20a43f7ac27ea
taseenjeem <taseenjeem.dev@gmail.com>
2024-08-19 00:44:49 +0600

    Update index.ts

--- database/db-queries/index.ts
+++ database/db-queries/index.ts
@@ -1,5 +1,5 @@
 export const getCategories = async () => {
-  return import("/database/json/category-list.json").then(
+  return import("@/database/json/category-list.json").then(
     (module) => module.default
   );
 };
