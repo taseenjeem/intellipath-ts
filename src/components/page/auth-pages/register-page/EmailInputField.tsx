b39f75da8e5402bf0233169af8ca4eed0328101e
taseenjeem <taseenjeem.dev@gmail.com>
2024-08-19 17:37:35 +0600

    Create EmailInputField.tsx

---
+++ src/components/page/auth-pages/register-page/EmailInputField.tsx
@@ -0,0 +1,42 @@
+import { RegisterFormInputs } from "@/types";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+
+interface EmailInputFieldProps {
+  register: UseFormRegister<RegisterFormInputs>;
+  errors: FieldErrors<RegisterFormInputs>;
+}
+
+const EmailInputField = ({ register, errors }: EmailInputFieldProps) => {
+  return (
+    <div className="form-control">
+      <label htmlFor="email" className="label">
+        <span className={`label-text ${errors?.email ? "text-error" : ""}`}>
+          Email
+        </span>
+      </label>
+      <input
+        {...register("email", {
+          required: "Your email is required",
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: "Please enter a valid email address",
+          },
+        })}
+        type="email"
+        id="email"
+        name="email"
+        placeholder="example@email.com"
+        className={`input input-bordered ${errors?.email ? "input-error" : ""}`}
+      />
+      {errors?.email && (
+        <div className="label">
+          <span className="label-text-alt text-error">
+            {errors.email.message}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EmailInputField;
