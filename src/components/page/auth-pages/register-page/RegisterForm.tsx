c830c8b55f82fbe7adb7ca2c49511c3d4899fa35
taseenjeem <taseenjeem.dev@gmail.com>
2024-08-19 17:40:31 +0600

    Create RegisterForm.tsx

---
+++ src/components/page/auth-pages/register-page/RegisterForm.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+import NameInputField from "./NameInputField";
+import EmailInputField from "./EmailInputField";
+import PassInputField from "./PassInputField";
+import { toast } from "react-toastify";
+import Logo from "@/src/components/global/ui/Logo";
+import { RegisterFormInputs } from "@/types";
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const methods = useForm<RegisterFormInputs>();
+
+  const handleRegister: SubmitHandler<RegisterFormInputs> = async (data) => {
+    setIsLoading(true);
+    try {
+      console.log(data);
+    } catch (error: any) {
+      toast.error(error.message);
+      methods.setError("root.serverError", { message: error.message });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(handleRegister)}
+        className="card-body pb-0"
+      >
+        <Logo formMode={true} />
+        <NameInputField
+          register={methods.register}
+          errors={methods.formState.errors}
+        />
+        <EmailInputField
+          register={methods.register}
+          errors={methods.formState.errors}
+        />
+        <PassInputField
+          register={methods.register}
+          errors={methods.formState.errors}
+        />
+        <div className="form-control mt-6">
+          {isLoading ? (
+            <button disabled className="btn btn-primary">
+              <span className="loading loading-spinner"></span>
+              Loading..
+            </button>
+          ) : (
+            <button type="submit" className="btn btn-primary">
+              Create an account
+            </button>
+          )}
+        </div>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default RegisterForm;
