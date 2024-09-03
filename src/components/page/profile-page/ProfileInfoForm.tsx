30b6c1a47da311b5450a42291446da260746fa7d
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-04 00:02:09 +0600

    Update ProfileInfoForm.tsx

--- src/components/page/profile-page/ProfileInfoForm.tsx
+++ src/components/page/profile-page/ProfileInfoForm.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import { ICountry, ILearnerInfo } from "@/types";
 import { RootState, useAppDispatch, useAppSelector } from "@/redux/store";
