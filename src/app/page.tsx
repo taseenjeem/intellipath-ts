86ca69bb72e746d918d287b275876206bc0e41c3
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-03 15:31:54 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/src/components/page/landing-page/Hero";
 import TopCourses from "../components/page/landing-page/TopCourses";
 import Testimonial from "../components/page/landing-page/Testimonial";
 import Cta from "../components/page/landing-page/Cta";
+import Partners from "../components/page/landing-page/Partnerts";
 
 const LandingPage = () => {
   return (
@@ -12,6 +13,7 @@ const LandingPage = () => {
       <TopCourses />
       <Testimonial />
       <Cta />
+      <Partners />
     </>
   );
 };
