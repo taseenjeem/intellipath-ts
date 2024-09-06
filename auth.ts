1b2664b69d872d6b6c22d888c3a4696804039608
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-06 23:01:55 +0600

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 import User from "./database/db-models/userModel";
 import { authConfig } from "./auth.config";
