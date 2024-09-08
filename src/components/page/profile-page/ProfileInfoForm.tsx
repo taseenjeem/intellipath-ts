42874695b26b5f9082b137a1462d53775b64a77b
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-08 14:58:06 +0600

    Update ProfileInfoForm.tsx

--- src/components/page/profile-page/ProfileInfoForm.tsx
+++ src/components/page/profile-page/ProfileInfoForm.tsx
@@ -77,9 +77,9 @@ const ProfileInfoForm = ({ userId, userEmail }: IProfileInfoFormProps) => {
 
   return (
     <div>
-      <h1 className="text-2xl font-semibold text-primary underline underline-offset-4 mb-5">
+      <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mb-5">
         Personal information
-      </h1>
+      </h3>
       <form onSubmit={handleSubmit} className="">
         <div className="">
           <div>
@@ -174,58 +174,78 @@ const ProfileInfoForm = ({ userId, userEmail }: IProfileInfoFormProps) => {
               />
             </div>
           </div>
-          {/* Email Input */}
-          <div className="form-control">
-            <label className="label" htmlFor="email">
-              <span className="label-text">Your Email</span>
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              className="input input-bordered cursor-not-allowed"
-              readOnly
-              value={formData.email ?? ""}
-              onChange={handleChange}
-            />
+          <hr className="md:my-10 my-5" />
+          <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mb-5">
+            Contact information
+          </h3>
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+            {/* Email Input */}
+            <div className="form-control">
+              <label className="label" htmlFor="email">
+                <span className="label-text">Your Email</span>
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="input input-bordered cursor-not-allowed"
+                readOnly
+                value={formData.email ?? ""}
+                onChange={handleChange}
+              />
+            </div>
+            {/* Phone Input */}
+            <div className="form-control">
+              <label className="label" htmlFor="phone">
+                <span className="label-text">Your Phone Number</span>
+              </label>
+              <input
+                id="phone"
+                name="phone"
+                type="number"
+                className="input input-bordered"
+                value={formData.phone ?? ""}
+                onChange={handleChange}
+              />
+            </div>
+            {/* Country Input */}
+            <div className="form-control">
+              <label className="label" htmlFor="country">
+                <span className="label-text">Your Country</span>
+              </label>
+              <select
+                id="country"
+                name="country"
+                className="select select-bordered"
+                value={formData.country ?? ""}
+                onChange={handleChange}
+              >
+                <option value="" disabled>
+                  Please select your country
+                </option>
+                {allCountries.map((item) => (
+                  <option key={item.code} value={item.name}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {/* Address Input */}
+            <div className="form-control">
+              <label className="label" htmlFor="address">
+                <span className="label-text">Your Address</span>
+              </label>
+              <input
+                id="address"
+                name="address"
+                type="text"
+                className="input input-bordered"
+                value={formData.address ?? ""}
+                onChange={handleChange}
+              />
+            </div>
           </div>
 
-          {/* Country Input */}
-          <div className="form-control">
-            <label className="label" htmlFor="country">
-              <span className="label-text">Your Country</span>
-            </label>
-            <select
-              id="country"
-              name="country"
-              className="select select-bordered"
-              value={formData.country ?? ""}
-              onChange={handleChange}
-            >
-              <option value="" disabled>
-                Please select your country
-              </option>
-              {allCountries.map((item) => (
-                <option key={item.code} value={item.name}>
-                  {item.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          {/* Phone Input */}
-          <div className="form-control">
-            <label className="label" htmlFor="phone">
-              <span className="label-text">Your Phone Number</span>
-            </label>
-            <input
-              id="phone"
-              name="phone"
-              type="number"
-              className="input input-bordered"
-              value={formData.phone ?? ""}
-              onChange={handleChange}
-            />
-          </div>
           {/* Expertise Input */}
           <div className="form-control">
             <label className="label" htmlFor="expertise">
@@ -262,20 +282,7 @@ const ProfileInfoForm = ({ userId, userEmail }: IProfileInfoFormProps) => {
             </div>
           </div>
         </div>
-        {/* Address Input */}
-        <div className="form-control">
-          <label className="label" htmlFor="address">
-            <span className="label-text">Your Address</span>
-          </label>
-          <input
-            id="address"
-            name="address"
-            type="text"
-            className="input input-bordered"
-            value={formData.address ?? ""}
-            onChange={handleChange}
-          />
-        </div>
+
         {/* Submit Button */}
         <div className="card-actions justify-end mt-5">
           {isLoading ? (
