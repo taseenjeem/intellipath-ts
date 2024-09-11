c32ca020b0dfc651b48d24f603a039ac014cd225
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-11 23:58:15 +0600

    separated contact info

---
+++ src/components/page/profile-page/EditContactInfo.tsx
@@ -0,0 +1,89 @@
+import { IUserInfo } from "@/types";
+
+interface IEditContactInfoProps {
+  formData: IUserInfo;
+  handleChange: (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
+  ) => void;
+}
+
+const EditContactInfo = ({ formData, handleChange }: IEditContactInfoProps) => {
+  return (
+    <>
+      <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mt-16 mb-5">
+        Contact information
+      </h3>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+        {/* Email Input */}
+        <div className="form-control">
+          <label className="label" htmlFor="email">
+            <span className="label-text">Your Email</span>
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="input input-bordered cursor-not-allowed"
+            readOnly
+            value={formData.email ?? ""}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Phone Input */}
+        <div className="form-control">
+          <label className="label" htmlFor="phone">
+            <span className="label-text">Your Phone Number</span>
+          </label>
+          <input
+            id="phone"
+            name="phone"
+            type="number"
+            className="input input-bordered"
+            value={formData.phone ?? ""}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Country Input */}
+        <div className="form-control">
+          <label className="label" htmlFor="country">
+            <span className="label-text">Your Country</span>
+          </label>
+          <select
+            id="country"
+            name="country"
+            className="select select-bordered"
+            value={formData.country ?? ""}
+            onChange={handleChange}
+          >
+            <option value="" disabled>
+              Please select your country
+            </option>
+            {allCountries.map((item) => (
+              <option key={item.code} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* Address Input */}
+        <div className="form-control">
+          <label className="label" htmlFor="address">
+            <span className="label-text">Your Address</span>
+          </label>
+          <input
+            id="address"
+            name="address"
+            type="text"
+            className="input input-bordered"
+            value={formData.address ?? ""}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditContactInfo;
--- src/components/page/profile-page/ProfileInfoForm.tsx
+++ src/components/page/profile-page/ProfileInfoForm.tsx
@@ -7,6 +7,7 @@ import { getUserByEmail, updateUserDetails } from "@/database/server-actions";
 import { toast } from "react-toastify";
 import { IoClose } from "react-icons/io5";
 import EditPersonalInfo from "./EditPersonalInfo";
+import EditContactInfo from "./EditContactInfo";
 
 const getCountries = async () => {
   return import("@/database/json/countries.json").then(
@@ -166,76 +167,7 @@ const ProfileInfoForm = ({ userId, userEmail }: IProfileInfoFormProps) => {
       <form onSubmit={handleSubmit} className="">
         <div className="">
           <EditPersonalInfo formData={formData} handleChange={handleChange} />
-          <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mt-16 mb-5">
-            Contact information
-          </h3>
-          <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
-            {/* Email Input */}
-            <div className="form-control">
-              <label className="label" htmlFor="email">
-                <span className="label-text">Your Email</span>
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="input input-bordered cursor-not-allowed"
-                readOnly
-                value={formData.email ?? ""}
-                onChange={handleChange}
-              />
-            </div>
-            {/* Phone Input */}
-            <div className="form-control">
-              <label className="label" htmlFor="phone">
-                <span className="label-text">Your Phone Number</span>
-              </label>
-              <input
-                id="phone"
-                name="phone"
-                type="number"
-                className="input input-bordered"
-                value={formData.phone ?? ""}
-                onChange={handleChange}
-              />
-            </div>
-            {/* Country Input */}
-            <div className="form-control">
-              <label className="label" htmlFor="country">
-                <span className="label-text">Your Country</span>
-              </label>
-              <select
-                id="country"
-                name="country"
-                className="select select-bordered"
-                value={formData.country ?? ""}
-                onChange={handleChange}
-              >
-                <option value="" disabled>
-                  Please select your country
-                </option>
-                {allCountries.map((item) => (
-                  <option key={item.code} value={item.name}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            {/* Address Input */}
-            <div className="form-control">
-              <label className="label" htmlFor="address">
-                <span className="label-text">Your Address</span>
-              </label>
-              <input
-                id="address"
-                name="address"
-                type="text"
-                className="input input-bordered"
-                value={formData.address ?? ""}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
+          <EditContactInfo formData={formData} handleChange={handleChange} />
           <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mt-16 mb-5">
             Academic Education
           </h3>
