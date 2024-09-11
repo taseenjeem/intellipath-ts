b1a2ab80d3a83c7da0ca80dfc7d51b946200e440
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-12 00:21:46 +0600

    separated the certifications

--- src/components/page/profile-page/ProfileInfoForm.tsx
+++ src/components/page/profile-page/ProfileInfoForm.tsx
@@ -9,6 +9,7 @@ import { IoClose } from "react-icons/io5";
 import EditPersonalInfo from "./EditPersonalInfo";
 import EditContactInfo from "./EditContactInfo";
 import EditAcademicInfo from "./EditAcademicInfo";
+import EditCertifications from "./EditCertifications";
 
 const getCountries = async () => {
   return import("@/database/json/countries.json").then(
@@ -180,121 +181,13 @@ const ProfileInfoForm = ({ userId, userEmail }: IProfileInfoFormProps) => {
             handleEducationChange={handleEducationChange}
             handleRemoveEducation={handleRemoveEducation}
           />
-          <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mt-16 mb-5">
-            Certifications
-          </h3>
-          <div>
-            {formData?.certifications && formData?.certifications.length > 0 ? (
-              <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
-                {formData?.certifications?.map((item) => (
-                  <div
-                    className="card card-body w-full h-full bg-base-100 hover:shadow-xl border border-base-300 hover:border-primary duration-300 relative"
-                    key={item.title}
-                  >
-                    <button
-                      type="button"
-                      className="bg-red-500 text-white p-1 rounded-full flex justify-end items-center absolute -top-2 -right-2"
-                      onClick={() => handleRemoveCertification(item.title)}
-                    >
-                      <IoClose />
-                    </button>
-
-                    <ul>
-                      <li className="text-xl font-bold">{item.title}</li>
-                      <hr className="my-3 border-gray-400" />
-                      <li>
-                        <strong>Issuer: </strong>
-                        {item.issuer}
-                      </li>
-                      <li>
-                        <strong>Date of Issue: </strong>
-                        {item.dateOfIssue}
-                      </li>
-                      <li>
-                        <strong>Show Credentials: </strong>
-                        <p className="line-clamp-1">
-                          {item.url ? item.url : "N/A"}
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <div className="border rounded-lg p-10 md:p-20 mt-5">
-                <p className="text-center">
-                  You have not added any education qualifications yet!
-                </p>
-              </div>
-            )}
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-5 items-end mt-5">
-              <div className="form-control">
-                <label className="label" htmlFor="title">
-                  <span className="label-text">Title</span>
-                </label>
-                <input
-                  id="title"
-                  name="title"
-                  type="text"
-                  className="input input-bordered"
-                  placeholder="EX: Certificate of Best Developer in 2024"
-                  value={certifications.title}
-                  onChange={handleCertificationChange}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label" htmlFor="issuer">
-                  <span className="label-text">Issuer</span>
-                </label>
-                <input
-                  id="issuer"
-                  name="issuer"
-                  type="text"
-                  className="input input-bordered"
-                  placeholder="Ex: University of Oxford"
-                  value={certifications.issuer}
-                  onChange={handleCertificationChange}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label" htmlFor="dateOfIssue">
-                  <span className="label-text">Date of issue</span>
-                </label>
-                <input
-                  id="dateOfIssue"
-                  name="dateOfIssue"
-                  type="text"
-                  className="input input-bordered"
-                  placeholder="Ex: 17 Feb 2016"
-                  value={certifications.dateOfIssue}
-                  onChange={handleCertificationChange}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label" htmlFor="url">
-                  <span className="label-text">Credential Link</span>
-                </label>
-                <input
-                  id="url"
-                  name="url"
-                  type="text"
-                  className="input input-bordered"
-                  placeholder="Ex: www.something.com"
-                  value={certifications.url}
-                  onChange={handleCertificationChange}
-                />
-              </div>
-              <div className="w-full">
-                <button
-                  type="button"
-                  className="btn btn-primary mt-3 w-full"
-                  onClick={handleAddCertification}
-                >
-                  Add New Certification
-                </button>
-              </div>
-            </div>
-          </div>
+          <EditCertifications
+            certifications={certifications}
+            formData={formData}
+            handleAddCertification={handleAddCertification}
+            handleCertificationChange={handleCertificationChange}
+            handleRemoveCertification={handleRemoveCertification}
+          />
           <h3 className="text-2xl font-semibold text-primary underline underline-offset-4 mt-16 mb-5">
             Skills & Expertise
           </h3>
