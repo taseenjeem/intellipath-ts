00557b9759aa7a55b9c9956650ce8555a9f859fd
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-14 01:15:03 +0600

    Update AddExperienceModal.tsx

--- src/components/page/profile-page/AddExperienceModal.tsx
+++ src/components/page/profile-page/AddExperienceModal.tsx
@@ -28,6 +28,55 @@ const AddExperienceModal = () => {
               <CgClose />
             </span>
           </div>
+          <div className="form-control">
+            <label className="label" htmlFor="companyName">
+              <span className="label-text">Company Name</span>
+            </label>
+            <input
+              id="companyName"
+              name="companyName"
+              type="text"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="designation">
+              <span className="label-text">Designation</span>
+            </label>
+            <input
+              id="designation"
+              name="designation"
+              type="text"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="location">
+              <span className="label-text">Location</span>
+            </label>
+            <input
+              id="location"
+              name="location"
+              type="text"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="period">
+              <span className="label-text">Period</span>
+            </label>
+            <input
+              id="period"
+              name="period"
+              type="text"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="w-full">
+            <button type="button" className="btn btn-primary mt-3 w-full">
+              Done
+            </button>
+          </div>
         </div>
       </dialog>
     </>
