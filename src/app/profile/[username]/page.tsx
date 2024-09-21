ffbdb8234cd23e6e7ce756fe68c3fafb8767ef87
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-21 15:01:36 +0600

    Update page.tsx

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -13,19 +13,14 @@ const MyAccountPage = async ({ params }: IMyAccountPageProps) => {
   const userData = await getUserByUsername(params.username);
 
   return (
-    <section className="custom-min-h container w-full">
-      <h1 className="lg:text-4xl text-3xl text-primary uppercase font-semibold mt-5">
-        My Profile
-      </h1>
-      <div role="tablist" className="tabs tabs-bordered w-full mt-5">
-        <ProfileInfoTab userData={userData} />
-        <CoursesTab />
-        <SettingsTab
-          userEmail={userData?.email ?? ""}
-          userId={userData?._id ?? ""}
-        />
-      </div>
-    </section>
+    <div role="tablist" className="tabs tabs-bordered w-full mt-5">
+      <ProfileInfoTab userData={userData} />
+      <CoursesTab />
+      <SettingsTab
+        userEmail={userData?.email ?? ""}
+        userId={userData?._id ?? ""}
+      />
+    </div>
   );
 };
 
