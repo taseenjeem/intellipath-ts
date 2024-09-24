d087fc008335a49e4ee57ccc127e9d59ed737a78
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-25 02:36:15 +0600

    Update ChangePassword.tsx

--- src/components/page/profile-page/ChangePassword.tsx
+++ src/components/page/profile-page/ChangePassword.tsx
@@ -2,8 +2,8 @@ import ChangePassForm from "./ChangePassForm";
 
 const ChangePassword = () => {
   return (
-    <section className="bg-base-200 p-4 md:p-6 mt-5 rounded-3xl">
-      <div className="grid grid-cols-2 gap-20">
+    <section className="bg-base-200 p-4 md:p-6 mt-10 md:mt-20 rounded-3xl">
+      <div className="grid md:grid-cols-2 gap-20">
         <div>
           <h3 className="text-2xl font-semibold text-primary mb-5">
             Change your password
