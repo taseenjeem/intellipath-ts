f01b09e84ca548655cf2e6c56bd225727596c710
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-25 16:34:20 +0600

    Update ProfileNavigation.tsx

--- src/components/page/profile-page/ProfileNavigation.tsx
+++ src/components/page/profile-page/ProfileNavigation.tsx
@@ -5,52 +5,55 @@ import { ImProfile } from "react-icons/im";
 import { IoIosBook, IoMdSettings } from "react-icons/io";
 
 const ProfileNavigation = ({ username }: { username: string }) => {
-  const pathName = usePathname();
+  const pathName = usePathname(); // Getting the current pathname
 
   return (
-    <>
-      <ul className="max-w-md bg-base-200 p-3 rounded-box flex flex-col md:flex-row items-center justify-between">
-        <li className="w-full md:w-auto">
-          <Link
-            href={`/profile/${username}`}
-            className={`${
-              pathName === `/profile/${username}`
-                ? "bg-primary text-base-100"
-                : ""
-            } flex items-center p-2 rounded-xl gap-2`}
-          >
-            <ImProfile size={24} />
-            My Information
-          </Link>
-        </li>
-        <li className="w-full md:w-auto">
-          <Link
-            href={`/profile/${username}/my-courses`}
-            className={`${
-              pathName === `/profile/${username}/my-courses`
-                ? "bg-primary text-base-100"
-                : ""
-            } flex items-center p-2 rounded-xl gap-2`}
-          >
-            <IoIosBook size={24} />
-            My Courses
-          </Link>
-        </li>
-        <li className="w-full md:w-auto">
-          <Link
-            href={`/profile/${username}/settings`}
-            className={`${
-              pathName === `/profile/${username}/settings`
-                ? "bg-primary text-base-100"
-                : ""
-            } flex items-center p-2 rounded-xl gap-2`}
-          >
-            <IoMdSettings size={24} />
-            Settings
-          </Link>
-        </li>
-      </ul>
-    </>
+    <ul className="max-w-md bg-base-200 p-3 rounded-box flex flex-col md:flex-row items-center justify-between">
+      {/* Navigation item for My Information */}
+      <li className="w-full md:w-auto">
+        <Link
+          href={`/profile/${username}`} // Link to the user's profile
+          className={`${
+            pathName === `/profile/${username}`
+              ? "bg-primary text-base-100"
+              : ""
+          } flex items-center p-2 rounded-xl gap-2`} // Conditional styling
+        >
+          <ImProfile size={24} /> {/* Profile icon */}
+          My Information
+        </Link>
+      </li>
+
+      {/* Navigation item for My Courses */}
+      <li className="w-full md:w-auto">
+        <Link
+          href={`/profile/${username}/my-courses`} // Link to user's courses
+          className={`${
+            pathName === `/profile/${username}/my-courses`
+              ? "bg-primary text-base-100"
+              : ""
+          } flex items-center p-2 rounded-xl gap-2`} // Conditional styling
+        >
+          <IoIosBook size={24} /> {/* Courses icon */}
+          My Courses
+        </Link>
+      </li>
+
+      {/* Navigation item for Settings */}
+      <li className="w-full md:w-auto">
+        <Link
+          href={`/profile/${username}/settings`} // Link to user settings
+          className={`${
+            pathName === `/profile/${username}/settings`
+              ? "bg-primary text-base-100"
+              : ""
+          } flex items-center p-2 rounded-xl gap-2`} // Conditional styling
+        >
+          <IoMdSettings size={24} /> {/* Settings icon */}
+          Settings
+        </Link>
+      </li>
+    </ul>
   );
 };
 
