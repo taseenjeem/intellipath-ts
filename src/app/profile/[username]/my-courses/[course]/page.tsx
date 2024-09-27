ccfb541b48adf8da3867231565abb08b1375412b
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-27 16:45:58 +0600

    Added breadcumb and many more

--- src/app/profile/[username]/my-courses/[course]/page.tsx
+++ src/app/profile/[username]/my-courses/[course]/page.tsx
@@ -1,5 +1,18 @@
-const PurchasedCourse = () => {
-  return <>Hello World</>;
+import { getCourseBySlug } from "@/database/db-queries";
+import Breadcrumbs from "@/src/components/page/purchased-course/Breadcrumbs";
+import VideoPlayer from "@/src/components/page/purchased-course/VideoPlayer";
+
+const PurchasedCourse = async ({
+  params,
+}: Readonly<{ params: { username: string; course: string } }>) => {
+  const course = await getCourseBySlug(params.course);
+
+  return (
+    <>
+      <Breadcrumbs username={params.username} course={course?.title ?? ""} />
+      <VideoPlayer />
+    </>
+  );
 };
 
 export default PurchasedCourse;
---
+++ src/components/page/purchased-course/VideoPlayer.tsx
@@ -0,0 +1,14 @@
+const VideoPlayer = () => {
+  return (
+    <>
+      <div className="mt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam in
+        reiciendis cumque quas at vero excepturi, repellendus sed necessitatibus
+        doloribus quia, cupiditate, harum voluptatum eaque. Eligendi in
+        consequatur odio velit.
+      </div>
+    </>
+  );
+};
+
+export default VideoPlayer;
