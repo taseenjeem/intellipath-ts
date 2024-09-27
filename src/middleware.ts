bca0eb043724875a20e90d3970393e51cd782f7f
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-27 15:54:02 +0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,22 +18,11 @@ export default auth((req) => {
     nextUrl.pathname.startsWith(route)
   );
 
-  // Check if the current path is part of the restricted routes
-  const isRestrictedRoute = AUTHENTICATED_RESTRICTED_ROUTES.find((route) =>
-    nextUrl.pathname.startsWith(route)
-  );
-
   // If the route is private and the user is not authenticated, redirect them to the login page
   if (isPrivateRoute && !isAuthenticated) {
     const redirectedUrl = new URL("/auth/login", nextUrl); // Set the redirect URL to the login page
     redirectedUrl.searchParams.set("message", "login_required"); // Add a message to indicate login is required
     return Response.redirect(redirectedUrl); // Redirect the user to the login page
-  }
-
-  // If the route is restricted and the user is authenticated, redirect them to home page
-  if (isRestrictedRoute && isAuthenticated) {
-    const redirectedUrl = new URL("/", nextUrl); // Set the redirect URL to home
-    return Response.redirect(redirectedUrl); // Redirect the user to the forbidden page
   }
 
   // If none of the above conditions are met, allow the request to proceed
