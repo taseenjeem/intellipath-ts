1f2ea683ccb8002b6e51a1b4d938e78dfe0c6374
taseenjeem <taseenjeem.dev@gmail.com>
2024-09-30 23:19:58 +0600

    added publish a new course page

--- src/app/profile/[username]/my-courses/page.tsx
+++ src/app/profile/[username]/my-courses/page.tsx
@@ -1,19 +1,33 @@
-import { getTopCourses } from "@/database/db-queries";
 import CourseCard from "@/src/components/global/ui/CourseCard";
-import { ICourses } from "@/types";
+import { ICourse } from "@/types";
 import illustrator from "/public/assets/images/search.png";
 import Image from "next/image";
+import { getUserByUsername } from "@/database/server-actions";
+import Link from "next/link";
 
 const MyCoursesPage = async ({
   params,
 }: Readonly<{ params: { username: string } }>) => {
-  const purchasedCourses: ICourses[] = await getTopCourses();
+  const { courses, role } = await getUserByUsername(params.username);
 
   return (
     <>
-      {purchasedCourses.length > 0 ? (
+      {role === "instructor" && (
+        <div className="my-5 space-y-2 flex justify-between">
+          <h3 className="text-xl md:text-2xl text-primary font-semibold">
+            You have published total {courses?.length} courses
+          </h3>
+          <Link
+            href={`/profile/${params.username}/my-courses/publish-a-new-course`}
+            className="btn btn-primary"
+          >
+            Publish a new course
+          </Link>
+        </div>
+      )}
+      {courses.length > 0 ? (
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-8 w-full mt-5">
-          {purchasedCourses.map((item: ICourses) => (
+          {courses.map((item: ICourse) => (
             <CourseCard
               key={item._id}
               href={`/profile/${params.username}/my-courses/${item.slug}`}
@@ -31,11 +45,14 @@ const MyCoursesPage = async ({
             alt="No purchased courses"
           />
           <h2 className="text-xl font-semibold mt-5">
-            You haven&apos;t purchased any courses yet.
+            {role === "learner"
+              ? "You haven't purchased any courses yet."
+              : "You haven't published any courses yet."}
           </h2>
           <p className="text-gray-600 mt-2">
-            Start exploring and learning new skills by purchasing courses from
-            our curated list.
+            {role === "learner"
+              ? "Start exploring and learning new skills by purchasing courses from our curated list."
+              : `To publish a course click on "Publish a new course" button`}
           </p>
         </div>
       )}
---
+++ src/app/profile/[username]/my-courses/publish-a-new-course/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const PublishCoursePage = () => {
+  return (
+    <>
+      <h1 className="text-xl md:text-2xl text-primary font-semibold my-5">
+        Publish a new course
+      </h1>
+    </>
+  );
+};
+
+export default PublishCoursePage;
