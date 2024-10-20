c74563237f1527f9436866c918c0c05ce27c2a60
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-21 00:53:34 +0600

    Update EditLessonModal.tsx

--- src/components/page/publish-a-new-course-page/EditLessonModal.tsx
+++ src/components/page/publish-a-new-course-page/EditLessonModal.tsx
@@ -3,7 +3,6 @@ import { editLessonTitle, editLessonURL } from "@/redux/slices/editCourse";
 import { useAppDispatch } from "@/redux/store";
 import { CgClose } from "react-icons/cg";
 import { useState, useEffect } from "react";
-import { toast } from "react-toastify";
 
 interface IEditLessonModalProps {
   lesson: {
@@ -17,11 +16,45 @@ const EditLessonModal = ({ lesson }: IEditLessonModalProps) => {
   const dispatch = useAppDispatch();
   const [title, setTitle] = useState(lesson.title);
   const [url, setUrl] = useState(lesson.url);
+  const [error, setError] = useState({ titleError: "", urlError: "" });
 
   useEffect(() => {
     setTitle(lesson.title);
     setUrl(lesson.url);
   }, [lesson]);
+
+  const isValidYouTubeUrl = (url: string) => {
+    const youtubeRegex =
+      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/gm;
+    return youtubeRegex.test(url);
+  };
+
+  const handleSaveChanges = () => {
+    let valid = true;
+
+    if (!title) {
+      setError((prev) => ({ ...prev, titleError: "Title is required" }));
+      valid = false;
+    } else {
+      setError((prev) => ({ ...prev, titleError: "" }));
+    }
+
+    if (!url || !isValidYouTubeUrl(url)) {
+      setError((prev) => ({
+        ...prev,
+        urlError: "Please enter a valid YouTube URL",
+      }));
+      valid = false;
+    } else {
+      setError((prev) => ({ ...prev, urlError: "" }));
+    }
+
+    if (valid) {
+      dispatch(editLessonTitle({ lessonId: lesson._id, newTitle: title }));
+      dispatch(editLessonURL({ lessonId: lesson._id, newURL: url }));
+      closeModal();
+    }
+  };
 
   const closeModal = () => {
     const editLessonModal = document.getElementById(
@@ -30,17 +63,9 @@ const EditLessonModal = ({ lesson }: IEditLessonModalProps) => {
     if (editLessonModal) {
       editLessonModal.close();
     }
-  };
-
-  const handleSaveChanges = () => {
-    if (!title || !url) {
-      toast.warn("You must provide a title or url");
-      return;
-    } else {
-      dispatch(editLessonTitle({ lessonId: lesson._id, newTitle: title }));
-      dispatch(editLessonURL({ lessonId: lesson._id, newURL: url }));
-      closeModal();
-    }
+    setTitle(lesson.title);
+    setUrl(lesson.url);
+    setError({ titleError: "", urlError: "" });
   };
 
   return (
@@ -58,31 +83,57 @@ const EditLessonModal = ({ lesson }: IEditLessonModalProps) => {
 
         <div className="form-control">
           <label htmlFor="title" className="label">
-            <span className="label-text">Lesson Title</span>
+            <span
+              className={`label-text ${error.titleError ? "text-error" : ""}`}
+            >
+              Lesson Title
+            </span>
           </label>
           <input
             type="text"
             id="title"
             name="title"
-            className="input input-bordered"
+            className={`input input-bordered ${
+              error.titleError ? "input-error" : ""
+            }`}
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
+          {error.titleError && (
+            <label htmlFor="title" className="label">
+              <span className="label-text text-error">{error.titleError}</span>
+            </label>
+          )}
         </div>
 
         <div className="form-control">
           <label htmlFor="url" className="label">
-            <span className="label-text">Lesson URL</span>
-            <span className="label-text">(YouTube Video)</span>
+            <span
+              className={`label-text ${error.urlError ? "text-error" : ""}`}
+            >
+              Lesson URL
+            </span>
+            <span
+              className={`label-text ${error.urlError ? "text-error" : ""}`}
+            >
+              (YouTube Video)
+            </span>
           </label>
           <input
             type="url"
             id="url"
             name="url"
-            className="input input-bordered"
+            className={`input input-bordered ${
+              error.urlError ? "input-error" : ""
+            }`}
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
+          {error.urlError && (
+            <label htmlFor="url" className="label">
+              <span className="label-text text-error">{error.urlError}</span>
+            </label>
+          )}
         </div>
 
         <button
