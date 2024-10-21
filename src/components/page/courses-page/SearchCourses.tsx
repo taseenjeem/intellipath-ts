5249d5646696f60abc5e0d2eeb1380141bf7ac3d
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-22 00:23:19 +0600

    Update SearchCourses.tsx

--- src/components/page/courses-page/SearchCourses.tsx
+++ src/components/page/courses-page/SearchCourses.tsx
@@ -1,9 +1,17 @@
+"use client";
 import { IoSearch } from "react-icons/io5";
 
 const SearchCourses = () => {
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <form className="input input-bordered max-w-sm w-full input-primary flex items-center gap-2">
+      <form
+        onSubmit={handleSearch}
+        className="input input-bordered max-w-sm w-full input-primary flex items-center gap-2"
+      >
         <input type="text" className="grow" placeholder="Search" />
         <button type="submit">
           <IoSearch size={18} className="text-primary" />
