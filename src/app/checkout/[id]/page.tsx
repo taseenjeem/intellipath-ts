272496493c8f11cce72af2c975d3825473551bf3
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-22 14:25:42 +0600

    Update page.tsx

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -2,7 +2,6 @@ import { getCourseById } from "@/database/server-actions";
 
 const CheckOutPage = async ({ params }: { params: { id: string } }) => {
   const course = await getCourseById(params.id);
-  console.log("🚀 ~ CheckOutPage ~ course:", course);
   return <>Hello World</>;
 };
 
