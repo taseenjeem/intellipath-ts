60ce6d683ce97524ceca178d2d6f3b88665ce544
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-22 14:18:47 +0600

    Update page.tsx

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -1,4 +1,8 @@
-const CheckOutPage = () => {
+import { getCourseById } from "@/database/server-actions";
+
+const CheckOutPage = async ({ params }: { params: { id: string } }) => {
+  const course = await getCourseById(params.id);
+  console.log("🚀 ~ CheckOutPage ~ course:", course);
   return <>Hello World</>;
 };
 
