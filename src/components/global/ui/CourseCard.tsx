327f6d26f3038093ea9a8aa5fc567b66e825423a
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-24 00:46:08 +0600

    Update CourseCard.tsx

--- src/components/global/ui/CourseCard.tsx
+++ src/components/global/ui/CourseCard.tsx
@@ -19,7 +19,7 @@ const CourseCard = ({
   href,
 }: CourseCardProps) => {
   return (
-    <div className="card card-compact bg-base-300 hover:shadow-xl border border-base-300 hover:border-primary duration-300 group">
+    <div className="card size-full card-compact bg-base-300 hover:shadow-xl border border-base-300 hover:border-primary duration-300 group">
       <figure className="w-full max-h-[202px] overflow-hidden">
         <Image
           src={courseDetails?.thumbnail}
