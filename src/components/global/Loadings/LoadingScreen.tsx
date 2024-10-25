f7f6f5c644652b51f8de7bd42552e6d4d184eb40
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-26 02:30:21 +0600

    Update LoadingScreen.tsx

--- src/components/global/Loadings/LoadingScreen.tsx
+++ src/components/global/Loadings/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import Logo from "../ui/Logo";
 const LoadingScreen = () => {
   return (
     <>
-      <section className="custom-min-h w-full flex flex-col justify-center items-center">
+      <section className="custom-min-h w-full flex flex-col justify-center items-center bg-base-100">
         <Logo loadingMode={true} />
         <progress className="progress w-60" />
       </section>
