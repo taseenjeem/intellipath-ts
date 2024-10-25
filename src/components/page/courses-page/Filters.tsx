c5822163944b70e7d89601477867adc5992e1024
taseenjeem <taseenjeem.dev@gmail.com>
2024-10-26 03:21:29 +0600

    Update Filters.tsx

--- src/components/page/courses-page/Filters.tsx
+++ src/components/page/courses-page/Filters.tsx
@@ -1,148 +1,64 @@
+import {
+  courseCategory,
+  courseLanguages,
+  courseLevel,
+} from "@/database/static-data";
+
 const Filters = () => {
   return (
     <>
       <aside className="w-1/5 hidden lg:block">
-        <div className="mt-10 p-3 rounded-lg bg-base-200 sticky top-[108px] border border-primary">
-          <h3 className="text-2xl font-semibold">Filters</h3>
-          <hr className="my-2" />
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
+        <div className="mt-10 p-3 rounded-lg bg-base-200 sticky top-[108px] border border-primary space-y-4">
+          <div>
+            <h3 className="text-2xl font-semibold">Categories</h3>
+            <hr className="my-2" />
+            {courseCategory.map((item) => (
+              <div key={item} className="form-control">
+                <label className="cursor-pointer label">
+                  <span className="label-text">{item}</span>
+                  <input
+                    id={item}
+                    name={item}
+                    type="checkbox"
+                    className="checkbox checkbox-sm checkbox-primary"
+                  />
+                </label>
+              </div>
+            ))}
           </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
+          <div>
+            <h3 className="text-2xl font-semibold">Languages</h3>
+            <hr className="my-2" />
+            {courseLanguages.map((item) => (
+              <div key={item} className="form-control">
+                <label className="cursor-pointer label">
+                  <span className="label-text">{item}</span>
+                  <input
+                    id={item}
+                    name={item}
+                    type="checkbox"
+                    className="checkbox checkbox-sm checkbox-primary"
+                  />
+                </label>
+              </div>
+            ))}
           </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <h3 className="text-2xl font-semibold">Filters</h3>
-          <hr className="my-2" />
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <h3 className="text-2xl font-semibold">Filters</h3>
-          <hr className="my-2" />
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="cursor-pointer label">
-              <span className="label-text">Filter 1</span>
-              <input
-                type="checkbox"
-                className="checkbox checkbox-sm checkbox-primary"
-              />
-            </label>
+          <div>
+            <h3 className="text-2xl font-semibold">Course Level</h3>
+            <hr className="my-2" />
+            {courseLevel.map((item) => (
+              <div key={item} className="form-control">
+                <label className="cursor-pointer label">
+                  <span className="label-text">{item}</span>
+                  <input
+                    id={item}
+                    name={item}
+                    type="checkbox"
+                    className="checkbox checkbox-sm checkbox-primary"
+                  />
+                </label>
+              </div>
+            ))}
           </div>
         </div>
       </aside>
