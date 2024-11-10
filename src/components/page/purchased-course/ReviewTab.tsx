fbf3c5b6cdae2e81f66ee9db9dd3e7bdd774fd15
taseenjeem <taseenjeem.dev@gmail.com>
2024-11-11 00:04:38 +0600

    Update ReviewTab.tsx

--- src/components/page/purchased-course/ReviewTab.tsx
+++ src/components/page/purchased-course/ReviewTab.tsx
@@ -6,6 +6,7 @@ import { getReviewByUsername } from "@/database/server-actions";
 import Image from "next/image";
 import userPlaceholder from "/public/assets/images/profile-placeholder.jpg";
 import { FaStar } from "react-icons/fa6";
+import LoadingScreen from "../../global/Loadings/LoadingScreen";
 
 interface IReviewTabProps {
   courseId: string;
@@ -45,49 +46,56 @@ const ReviewTab = ({ courseId, username }: IReviewTabProps) => {
         aria-label="Reviews"
       />
       <div role="tabpanel" className="tab-content pt-5 min-h-[60vh]">
-        {review && (
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
           <>
-            <h3 className="mb-5 font-semibold">Thank you for your response</h3>
-            <blockquote className="card p-5 bg-base-300 hover:shadow-xl border border-base-300 hover:border-primary duration-300">
-              <div className="flex items-center gap-4">
-                <div className="avatar">
-                  <div className="ring-primary ring-offset-base-100 rounded-full ring ring-offset-2">
-                    <Image
-                      alt="Profile Picture"
-                      src={
-                        typeof review.user !== "string" &&
-                        review.user.profileImageUrl
-                          ? review.user.profileImageUrl
-                          : userPlaceholder
-                      }
-                      width={48}
-                      height={48}
-                      className="size-14 rounded-full object-cover"
-                    />
+            {review && (
+              <>
+                <h3 className="mb-5 font-semibold">
+                  Thank you for your response
+                </h3>
+                <blockquote className="card p-5 bg-base-300 hover:shadow-xl border border-base-300 hover:border-primary duration-300">
+                  <div className="flex items-center gap-4">
+                    <div className="avatar">
+                      <div className="ring-primary ring-offset-base-100 rounded-full ring ring-offset-2">
+                        <Image
+                          alt="Profile Picture"
+                          src={
+                            typeof review.user !== "string" &&
+                            review.user.profileImageUrl
+                              ? review.user.profileImageUrl
+                              : userPlaceholder
+                          }
+                          width={48}
+                          height={48}
+                          className="size-14 rounded-full object-cover"
+                        />
+                      </div>
+                    </div>
+
+                    <div>
+                      <p className="font-semibold text-lg">
+                        {typeof review.user !== "string"
+                          ? `${review.user.firstName} ${review.user.lastName}`
+                          : "Unknown User"}
+                      </p>
+                      <span className="flex items-center text-sm gap-2 text-accent">
+                        {review.rating}
+                        <FaStar />
+                      </span>
+                    </div>
                   </div>
-                </div>
 
-                <div>
-                  <p className="font-semibold text-lg">
-                    {typeof review.user !== "string"
-                      ? `${review.user.firstName} ${review.user.lastName}`
-                      : "Unknown User"}
+                  <p className="mt-4 italic text-xs md:text-base">
+                    &quot; {review.content} &quot;
                   </p>
-                  <span className="flex items-center text-sm gap-2 text-accent">
-                    {review.rating}
-                    <FaStar />
-                  </span>
-                </div>
-              </div>
-
-              <p className="mt-4 italic text-xs md:text-base">
-                &quot; {review.content} &quot;
-              </p>
-            </blockquote>
+                </blockquote>
+              </>
+            )}
+            {!review && <AddReviewForm courseId={courseId} />}
           </>
         )}
-
-        {!review && <AddReviewForm courseId={courseId} />}
       </div>
     </>
   );
