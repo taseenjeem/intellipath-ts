fc7db0ac84b5b8ce2989a90a26aa41ba5d143d9a
taseenjeem <taseenjeem.dev@gmail.com>
2024-11-17 22:56:31 +0600

    Update page.tsx

--- src/app/profile/[username]/my-courses/edit-course/[course]/page.tsx
+++ src/app/profile/[username]/my-courses/edit-course/[course]/page.tsx
@@ -1,6 +1,36 @@
-import { getCourseBySlug } from "@/database/server-actions";
+import {
+  getAllCourses,
+  getAllUsers,
+  getCourseBySlug,
+} from "@/database/server-actions";
 import Breadcrumbs from "@/src/components/global/ui/Breadcrumbs";
 import EditCourseForm from "@/src/components/page/edit-course/EditCourseForm";
+import { ICourse, IUserInfo } from "@/types";
+import { Metadata } from "next";
+
+export const generateStaticParams = async () => {
+  const allUsers: IUserInfo[] = await getAllUsers();
+  const allCourses: ICourse[] = await getAllCourses();
+
+  const staticPaths: any = [];
+
+  allUsers.forEach((user: IUserInfo) => {
+    allCourses.forEach((course: ICourse) => {
+      staticPaths.push({
+        params: {
+          username: user.username,
+          course: course.slug,
+        },
+      });
+    });
+  });
+
+  return staticPaths;
+};
+
+export const metadata: Metadata = {
+  title: "Edit Course - Intellipath",
+};
 
 const EditCoursePage = async ({
   params,
