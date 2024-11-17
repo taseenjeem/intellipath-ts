3594b1677a50824856179c90687d36e9eb060e4f
taseenjeem <taseenjeem.dev@gmail.com>
2024-11-17 23:12:49 +0600

    Update page.tsx

--- src/app/enroll-success/page.tsx
+++ src/app/enroll-success/page.tsx
@@ -12,6 +12,7 @@ import {
 import { redirect } from "next/navigation";
 import { auth } from "@/auth";
 import { stripe } from "@/services/stripe/stripeConfig";
+import { Metadata } from "next";
 
 interface EnrollSuccessPageProps {
   searchParams: {
@@ -20,6 +21,12 @@ interface EnrollSuccessPageProps {
     instructorId: string;
   };
 }
+
+export const metadata: Metadata = {
+  title: "Enrollment Successful - Intellipath",
+  description:
+    "IntelliPath is a cutting-edge online education platform designed to empower learners of all ages with personalized and engaging learning experiences. Our platform offers a vast array of courses across various disciplines, utilizing advanced technology to tailor learning paths that suit individual needs and goals. Whether you're looking to advance your career, develop new skills, or explore new interests, IntelliPath provides the tools and support to help you succeed.",
+};
 
 const EnrollSuccessPage = async ({ searchParams }: EnrollSuccessPageProps) => {
   const userSession = await auth();
